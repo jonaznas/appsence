b07e01577daa0bf7b9a9d981dd3657e1b1eea832
jonaznas <git@jonaz.dev>
2022-02-28 22:46:47 +0100

    fix(client): missing endpoint environment in production

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   apiUrl: 'https://missing-times.herokuapp.com',
+  endpoint: {
+    userProfile: '/v1/user/profile'
+  },
   supabase: {
     url: 'https://kgjzqbfwrbwqspwmqjqx.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnanpxYmZ3cmJ3cXNwd21xanF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDUxODQ5NzcsImV4cCI6MTk2MDc2MDk3N30.TqDQpL_iDHMVqVhJCPGsPuUlEYVYGVEORjTCkDhDpow'
