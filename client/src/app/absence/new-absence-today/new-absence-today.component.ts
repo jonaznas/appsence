4afa0ea232e99582a28ee71a44d9b9a93a17a440
jonaznas <git@jonaz.dev>
2022-03-03 08:27:49 +0100

    feat(client): start with absence pages

---
+++ client/src/app/absence/new-absence-today/new-absence-today.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-absence-today',
+  templateUrl: './new-absence-today.component.html',
+  styleUrls: ['./new-absence-today.component.scss']
+})
+export class NewAbsenceTodayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/absence/new-absence/new-absence-date/new-absence-date.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-absence-date',
+  templateUrl: './new-absence-date.component.html',
+  styleUrls: ['./new-absence-date.component.scss']
+})
+export class NewAbsenceDateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
